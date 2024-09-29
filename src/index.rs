8ad3b49d94015a120e0784d44c66ad598c35846d
Nail Sharipov <nailxsharipov@gmail.com>
2024-09-29 20:05:11 +0300

    switch to template api

--- src/index.rs
+++ src/index.rs
@@ -1,16 +1,68 @@
-pub struct BinLayout {
-    pub(super) min_key: i64,
-    pub(super) power: usize
+use std::ops::Range;
+
+pub trait Offset {
+    fn offset(self, other: Self) -> usize;
 }
 
-impl BinLayout {
+impl Offset for i64 {
     #[inline(always)]
-    pub fn index(&self, value: i64) -> usize {
-        ((value - self.min_key) as usize) >> self.power
+    fn offset(self, other: Self) -> usize {
+        self.wrapping_sub(other) as usize
     }
 }
 
-pub trait BinKey {
-    fn key(&self) -> i64;
-    fn bin(&self, layout: &BinLayout) -> usize;
+impl Offset for i32 {
+    #[inline(always)]
+    fn offset(self, other: Self) -> usize {
+        (self as i64).wrapping_sub(other as i64) as usize
+    }
+}
+
+impl Offset for usize {
+    #[inline(always)]
+    fn offset(self, other: Self) -> usize {
+        self.wrapping_sub(other)
+    }
+}
+
+pub struct BinLayout<T> {
+    pub(super) min_key: T,
+    pub(super) power: usize,
+}
+
+impl<T> BinLayout<T>
+where
+    T: Copy + Offset + PartialOrd,
+{
+    #[inline(always)]
+    pub fn index(&self, value: T) -> usize {
+        value.offset(self.min_key) >> self.power
+    }
+
+    #[inline(always)]
+    pub fn new(range: Range<T>, elements_count: usize) -> Option<BinLayout<T>> {
+        let delta = range.end.offset(range.start) + 1;
+        let max_possible_bin_count = delta.min(elements_count >> 1).min(8192);
+        if max_possible_bin_count <= 1 {
+            return None;
+        }
+
+        let scale = delta / max_possible_bin_count;
+        let scale_power = log2(scale);
+        Some(Self {
+            min_key: range.start,
+            power: scale_power,
+        })
+    }
+}
+
+#[inline(always)]
+fn log2(value: usize) -> usize {
+    let n = value.leading_zeros();
+    (usize::BITS - n) as usize
+}
+
+pub trait BinKey<T> {
+    fn bin_key(&self) -> T;
+    fn bin_index(&self, layout: &BinLayout<T>) -> usize;
 }
--- src/key_sort.rs
+++ src/key_sort.rs
@@ -1,5 +1,5 @@
 use std::cmp::Ordering;
-use crate::index::{BinKey, BinLayout};
+use crate::index::{BinKey, BinLayout, Offset};
 
 #[derive(Debug, Clone)]
 pub struct Bin {
@@ -7,7 +7,7 @@ pub struct Bin {
     pub data: usize,
 }
 
-pub trait KeyBinSort {
+pub trait KeyBinSort<T> {
     type Item;
     fn sort_by_bins(&mut self) -> Vec<Bin>;
     fn sort_with_bins<F>(&mut self, compare: F)
@@ -18,12 +18,16 @@ pub trait KeyBinSort {
         F: Fn(&Self::Item, &Self::Item) -> Ordering;
 }
 
-impl<T: BinKey + Clone> KeyBinSort for [T] {
+impl<T, U> KeyBinSort<U> for [T]
+where
+    T: BinKey<U> + Clone,
+    U: Copy + Ord + Offset,
+{
     type Item = T;
 
     fn sort_by_bins(&mut self) -> Vec<Bin> {
         let mut min_key = if let Some(item) = self.first() {
-            item.key()
+            item.bin_key()
         } else {
             return vec![];
         };
@@ -31,27 +35,22 @@ impl<T: BinKey + Clone> KeyBinSort for [T] {
         let mut max_key = min_key;
 
         for p in self.iter() {
-            let key = p.key();
+            let key = p.bin_key();
             min_key = key.min(min_key);
             max_key = key.max(max_key);
         }
 
-        let delta = (max_key - min_key) as usize;
-        let max_possible_bin_count = delta.min(self.len() >> 1).min(8192);
-        if max_possible_bin_count <= 1 {
+        let layout = if let Some(layout) = BinLayout::new(min_key..max_key, self.len()) {
+            layout
+        } else {
             return vec![Bin { offset: 0, data: self.len() }];
-        }
-
-        let scale = delta / max_possible_bin_count;
-        let scale_power = log2(scale);
-        let layout = BinLayout { min_key, power: scale_power };
+        };
 
         let bin_count = layout.index(max_key) + 1;
-
         let mut bins = vec![Bin { offset: 0, data: 0 }; bin_count];
 
         for p in self.iter() {
-            let index = p.bin(&layout);
+            let index = p.bin_index(&layout);
             unsafe { bins.get_unchecked_mut(index) }.data += 1;
         }
 
@@ -65,16 +64,17 @@ impl<T: BinKey + Clone> KeyBinSort for [T] {
 
         let copy = self.to_vec();
 
-        for p in copy.into_iter() {
-            let index = p.bin(&layout);
+        for item in copy.into_iter() {
+            let index = item.bin_index(&layout);
             let bin = unsafe { bins.get_unchecked_mut(index) };
-            *unsafe { self.get_unchecked_mut(bin.data) } = p;
+            *unsafe { self.get_unchecked_mut(bin.data) } = item;
             bin.data += 1;
         }
 
         bins
     }
 
+    #[inline]
     fn sort_with_bins<F>(&mut self, compare: F)
     where
         F: Fn(&T, &T) -> Ordering,
@@ -94,6 +94,7 @@ impl<T: BinKey + Clone> KeyBinSort for [T] {
         }
     }
 
+    #[inline]
     fn sort_unstable_with_bins<F>(&mut self, compare: F)
     where
         F: Fn(&T, &T) -> Ordering,
@@ -112,125 +113,5 @@ impl<T: BinKey + Clone> KeyBinSort for [T] {
                 self[start..end].sort_unstable_by(|a, b| compare(a, b));
             }
         }
-    }
-}
-
-fn log2(value: usize) -> usize {
-    let n = value.leading_zeros();
-    (usize::BITS - n) as usize
-}
-
-#[cfg(test)]
-mod tests {
-    use std::cmp::Ordering;
-    use std::ops::Range;
-    use rand::Rng;
-    use crate::index::{BinKey, BinLayout};
-    use crate::key_sort::KeyBinSort;
-
-    #[derive(Debug, Clone, Copy, PartialEq, Eq)]
-    struct Point {
-        x: i32,
-        y: i32,
-    }
-
-    impl Point {
-        fn new(x: i32, y: i32) -> Self {
-            Point { x, y }
-        }
-    }
-
-    impl PartialOrd for Point {
-        #[inline(always)]
-        fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-            Some(self.cmp(other))
-        }
-    }
-
-    impl Ord for Point {
-        #[inline(always)]
-        fn cmp(&self, other: &Self) -> Ordering {
-            let x = self.x == other.x;
-            if x && self.y == other.y {
-                Ordering::Equal
-            } else if self.x < other.x || x && self.y < other.y {
-                Ordering::Less
-            } else {
-                Ordering::Greater
-            }
-        }
-    }
-
-    impl BinKey for Point {
-        #[inline(always)]
-        fn key(&self) -> i64 {
-            self.x as i64
-        }
-
-        #[inline(always)]
-        fn bin(&self, layout: &BinLayout) -> usize {
-            layout.index(self.key())
-        }
-    }
-
-    #[test]
-    fn test_0() {
-        let points = vec![
-            Point::new(13, 2),
-            Point::new(1, 2),
-            Point::new(10, 2),
-            Point::new(4, 0),
-            Point::new(8, 0),
-            Point::new(7, 4),
-            Point::new(8, 2),
-            Point::new(10, 2),
-            Point::new(14, 2),
-        ];
-
-        let mut ordered = points.clone();
-        ordered.sort_unstable_by(|a, b| a.cmp(b));
-
-        let mut result = points.clone();
-        let bins = result.sort_by_bins();
-
-        // Sort each bin using the provided comparison function
-        for bin in bins.iter() {
-            let start = bin.offset;
-            let end = bin.data;
-            if start < end {
-                result[start..end].sort_unstable_by(|a, b| a.cmp(b));
-            }
-        }
-
-        assert_eq!(ordered, result);
-    }
-
-    #[test]
-    fn test_random() {
-        for i in 0..1000 {
-            let points = random_points(i, -1000..1000, -1000..1000);
-            let mut arr_0 = points.clone();
-            arr_0.sort_by(|a, b| a.cmp(b));
-
-            let mut arr_1 = points.clone();
-            arr_1.sort_with_bins(|a, b| a.cmp(b));
-
-            let mut arr_2 = points.clone();
-            arr_2.sort_unstable_with_bins(|a, b| a.cmp(b));
-
-            assert_eq!(arr_0, arr_1);
-        }
-    }
-
-    fn random_points(n: usize, x_range: Range<i32>, y_range: Range<i32>) -> Vec<Point> {
-        let mut points = Vec::with_capacity(n);
-        let mut rng = rand::thread_rng();
-        for _ in 0..n {
-            let x = rng.gen_range(x_range.clone());
-            let y = rng.gen_range(y_range.clone());
-            points.push(Point { x, y })
-        }
-
-        points
     }
 }
